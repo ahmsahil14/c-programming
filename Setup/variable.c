d1f0d1ac1c9de4ba3d314692786cfe33ab14ed5b
unknown <ahmsahil14>
2023-04-03 20:16:46 +0600

    variable.c

--- Setup/variable.c
+++ Setup/variable.c
@@ -4,6 +4,6 @@ int main()
     int rahim=10;
     float karim=1000.50000;
     char zahid= 'Z';
-    printf("%d %0.3f %c",rahim,karim,zahid);
+    printf("%d\n%0.3f\n %c",rahim,karim,zahid);
     return 0;
 }
