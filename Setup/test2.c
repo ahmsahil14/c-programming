dc584c3614f0cf9b67c4097e62daa0865be1e6e1
unknown <ahmsahil14>
2023-04-07 01:36:53 +0600

    test2.c

---
+++ Setup/test2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int taka;
+    scanf("%d",&taka);
+    if(taka>=100)
+    {
+        printf("Askey biri kaboo must");
+    }
+    else if(taka<50)
+    {
+        printf("Derby khabo taka kom amr");
+    }
+    else
+    {
+        printf("Askey biri kaboo nah");
+    
+    }
+    return 0;
+}
--- Setup/variable.c
+++ Setup/variable.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int rahim=10;
-    float karim=1000.50000;
-    char zahid= 'Z';
-    printf("%d\n%0.3f\n %c",rahim,karim,zahid);
+    int rahim,karim;
+    scanf("%d")
+   
+    printf("%d\n%0.3f\n%c",rahim,karim,zahid);
     return 0;
 }
